6e92e1ee7c53e60c125fb9f79634079c2ea4e70d
杜学渊 <duxueyuan@10-18-229-200.dynapool.wireless.nyu.edu>
2022-02-19 16:11:24 -0500

    Same as Professor

---
+++ Menu.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  PairTrading
+//
+//  Created by 杜学渊 on 2/19/22.
+//
+
+
+#include "Trade.hpp"
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Menu" << endl;
+    cout << "A - Create and Populate Pair Table\n"
+            "B - Retrieve and Populate Historical Data for Each Stock\n"
+            "C - Create PairPrices Table\n"
+            "D - Calculate Volatility\n"
+            "E - Back Test\n"
+            "F - Calculate Profit and Loss For Each Pair\n"
+            "G - Manual Testing\n"
+            "H - Drop All the Tables\n"
+            "X - Exit" << endl;
+    
+    return 0;
+}
---
+++ Trade.cpp
@@ -0,0 +1,9 @@
+//
+//  Trade.cpp
+//  PairTrading
+//
+//  Created by 杜学渊 on 2/19/22.
+//
+
+
+//#include "Trade.hpp"
