97a3c3d129f098a86f0f736b9857b8eae3504c31
qinyyyy <88140638+qinyyyy@users.noreply.github.com>
2022-02-27 11:55:22 -0500

    Update Util.h

--- Util.h
+++ Util.h
@@ -8,3 +8,5 @@ map<string, string> ProcessConfigData(string config_file);
 
 //writing call back function for storing fetched values in memory
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+
+map<string, string> GetPairs(string pair_file);
